7bda04bfe3402165986c63331e7b4549a4cee02b
Alessandro Elias <ae11@c3sl.ufpr.br>
2017-09-24 23:08:25 -0300

    Working.
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- prox-subconj.c
+++ prox-subconj.c
@@ -17,10 +17,51 @@
  * =====================================================================================
  */
 
-#include <stdio.h>
+#include <iostream>
+#include <vector>
 
-/*_____________________________________________________________________________________*/
+using namespace std;
+
+typedef vector<int> VINT;
+
+VINT set;
+VINT s;
+VINT combinations;
+
+void print(const VINT& v) {
+  unsigned int i;
+  static bool found = false;
+
+  if( found ) {
+    for( i = 0; i < v.size(); ++i )
+      cout << v[i] << " ";
+    cout << endl;
+  }
+  found = (v == s);
+}
+
+void generate(int offset, int k) {
+  if (k == 0) {
+    print(combinations);
+    return;
+  }
+
+  for( unsigned int i = offset; i <= set.size() - k; ++i ) {
+    combinations.push_back(set[i]);
+    generate(i+1, k-1);
+    combinations.pop_back();
+  }
+}
+
 int main(int argc, char *argv[]) {
+  int n = atoi(argv[1]), k = atoi(argv[2]);
+
+  for( int i = 0; i < n; ++i )
+    set.push_back(i+1);
+  for( int i = 3; i <= k+2; i++ )
+    s.push_back(atoi(argv[i]));
+
+  generate(0, k);
 
   return 0;
 }
