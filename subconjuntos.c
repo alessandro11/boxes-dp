6ae1d89da7281ccfdcbd52b1619157930cb145de
Alessandro Elias <ae11@c3sl.ufpr.br>
2017-09-24 21:53:00 -0300

    version do deliver.
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- subconjuntos.c
+++ subconjuntos.c
@@ -16,10 +16,40 @@
  * =====================================================================================
  */
 
-#include <stdio.h>
+#include <iostream>
+#include <vector>
 
-/*_____________________________________________________________________________________*/
+using namespace std;
+
+typedef vector<int> VINT;
+
+VINT set;
+VINT combinations;
+
+void print(const VINT& v) {
+  for( unsigned int i = 0; i < v.size(); ++i )
+    cout << v[i] << " ";
+  cout << endl;
+}
+
+void generate(int offset, int k) {
+  if (k == 0) {
+    print(combinations);
+    return;
+  }
+
+  for( unsigned int i = offset; i <= set.size() - k; ++i ) {
+    combinations.push_back(set[i]);
+    generate(i+1, k-1);
+    combinations.pop_back();
+  }
+}
+
 int main(int argc, char *argv[]) {
+  int n = atoi(argv[1]), k = atoi(argv[2]);
+
+  for (int i = 0; i < n; ++i) { set.push_back(i+1); }
+  generate(0, k);
 
   return 0;
 }
